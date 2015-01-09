9f8e5792b059f532278719ce44564ecb1b71c9e8
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2015-01-09 23:35:52 +0100

    - gerade gezogen

--- konsolen_tools/lan64_send/lan64_send.pro
+++ konsolen_tools/lan64_send/lan64_send.pro
@@ -1,10 +1,31 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-01-09T22:23:51
+#
+#-------------------------------------------------
+
+QT       -= core
+QT       -= gui
+
+TARGET = lan64_send
+CONFIG   += console
+CONFIG   -= app_bundle
+
 TEMPLATE = app
-CONFIG += console
-CONFIG -= app_bundle
-CONFIG -= qt
 
 SOURCES += main.cpp
 
-include(deployment.pri)
-qtcAddDeployment()
+linux-g++-32{
+DESTDIR = "../../bin/linux_32bit"
+LIBS += -lusb
+}
 
+linux-g++-64{
+DESTDIR = "../../bin/linux_64bit"
+LIBS += -lusb
+}
+
+win32-g++{
+DESTDIR = "../../bin/win_x32"
+LIBS += -lhid -lsetupapi
+}
--- konsolen_tools/lan64_send/main.cpp
+++ konsolen_tools/lan64_send/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main()
+void help_out(void);
+
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    /// Comandline auswerten ///
+
+    bool error = false;
+
+    if(argc > 1)
+    {
+        if(strcmp(argv[1],"--help") == 0)
+        {
+            help_out();
+            return (0);
+        }
+
+        if(strcmp(argv[1],"--version") == 0)
+        {
+            cout << endl << "Version 0.10 [Code by Thorsten Kattanek (C)2014]" << endl;
+            return (0);
+        }
+    }
+
+    if(argc > 2 || argc == 1)
+    {
+        help_out();
+        return (0);
+    }
+
+
+    // Datei Übertragen
 }
 
+void help_out(void)
+{
+    cout << endl << "usage: lan64_send 'c64 file'" << endl;
+    cout << "Tranfer a C64 File to C64 over the LAN64 USB" << endl << endl;
+    cout << "--help\t\tThis shows the help" << endl;
+    cout << "--version\tThis shows the version information" << endl;
+}
